9e2733c3d699111b317aed20efb2c11715a737c0
Neel Patel <npatel@college.harvard.edu>
2013-01-20 21:56:45 -0500

    Update number 2 for email conf

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,19 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Initialize the rails application
-Biz::Application.initialize!
-
 ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
    :tls => true,
    :address => "smtp.gmail.com",
    :port => 587,
    :domain => "gmail.com",
-   :authentication => :login,
-   :user_name => "npatel@college.harvard.edu",
-   :password => "1613ononNARAN123"
+   :authentication => "plain",
+   :user_name => "got2surf@gmail.com",
+   :password => "naran123"
+   :enable_starttls_auto => true
+
  }
+
+# Initialize the rails application
+Biz::Application.initialize!
+
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Biz::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
-     :port => 587,
+     :port => "587",
      :domain => "gmail.com",
      :authentication => :login,
      :user_name => "npatel@college.harvard.edu",
