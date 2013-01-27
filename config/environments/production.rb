539bb0e6b215a49970cd980753af1b3ada5e382f
Neel Patel <npatel@college.harvard.edu>
2013-01-27 07:12:11 -0500

    Update with correct mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Biz::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.default_url_options = {:host => 'lit-caverns-8821.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
